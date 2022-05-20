6459654a716adde29cf9324846871a4d0fd118d4
chaeyeoneee <343265@naver.com>
2022-05-20 11:19:14 +0900

    change

--- flask_project/__init__.py
+++ flask_project/__init__.py
@@ -9,3 +9,11 @@ def main():
 @app.route("/myinfo")
 def myinfo():
     return render_template("myinfo.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
